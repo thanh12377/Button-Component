28cb7acdedb31c3ac9719ae49d2aec2f042b3cc4
unknown <you@example.com>
2022-06-14 23:20:16 +0700

    New btn-component

--- src/App.js
+++ src/App.js
@@ -1,133 +1,84 @@
 import React from "react";
 import Button from "./UI/Button";
-import classes from "./App.module.css";
-import { BsFillCartPlusFill } from "react-icons/bs";
+
 
 function App() {
   return (
     <div className="App">
-      <div className={classes.container}>
-        <div className={classes["btn-box"]}>
-          <div className={classes.btn}>
-            <span>&lsaquo;button /&rsaquo;</span>
-            <Button className={classes.default}>Default</Button>
-          </div>
-          <div className={classes.btn}>
-            <span className={classes.hoverText}>&#38;:hover, &#38;:focus</span>
-            <Button className={classes["default-hover"]}>Default</Button>
-          </div>
-        </div>
-        <div className={classes["btn-box"]}>
-          <div className={classes.btn}>
-            <span>&lsaquo;button variant="outline" /&rsaquo;</span>
-            <Button className={classes.outline}>Default</Button>
-          </div>
-          <div className={classes.btn}>
-            <span className={classes.hoverText}>&#38;:hover, &#38;:focus</span>
-            <Button className={classes["outline-hover"]}>Default</Button>
-          </div>
-        </div>
-        <div className={classes["btn-box"]}>
-          <div className={classes.btn}>
-            <span>&lsaquo;button variant="text" /&rsaquo;</span>
-            <Button className={classes.text}>Default</Button>
-          </div>
-          <div className={classes.btn}>
-            <span className={classes.hoverText}>&#38;:hover, &#38;:focus</span>
-            <Button className={classes["text-hover"]}>Default</Button>
-          </div>
-        </div>
-        <div className={classes["btn-box"]}>
-          <div className={classes.btn}>
-            <span>&lsaquo;button disableShadow /&rsaquo;</span>
-            <Button className={classes.primary}>Default</Button>
-          </div>
-        </div>
-        <div className={classes["btn-box"]}>
-          <div className={classes.btn}>
-            <span>&lsaquo;button disabled/&rsaquo;</span>
-            <Button className={classes.disable} disable>
-              Disabled
-            </Button>
-          </div>
-          <div className={classes.btn}>
-            <span>&lsaquo;button variant="text" disabled/&rsaquo;</span>
-            <Button className={classes["disabled-hover"]}>Disabled</Button>
-          </div>
-        </div>
-        <div className={classes["btn-box"]}>
-          <div className={classes.btn}>
-            <span>&lsaquo;button startIcon="local-grocery-store"/&rsaquo;</span>
-            <Button className={`${classes.grocery} ${classes.primary}`}>
-              <BsFillCartPlusFill className={classes.icon} />
-              Default
-            </Button>
-          </div>
-          <div className={classes.btn}>
-            <span>&lsaquo;button endIcon="local-grocery-store"/&rsaquo;</span>
-            <Button className={`${classes.grocery} ${classes.primary}`}>
-              Default
-              <BsFillCartPlusFill className={classes.icon} />
-            </Button>
-          </div>
-        </div>
-        <div className={classes["btn-box"]}>
-          <div className={classes.btn}>
-            <span>&lsaquo;button size="sm" /&rsaquo;</span>
-            <Button className={`${classes.sm} ${classes.primary}`}>
-              Default
-            </Button>
-          </div>
-          <div className={classes.btn}>
-            <span>&lsaquo;button size="md" /&rsaquo;</span>
-            <Button className={`${classes.md} ${classes.primary}`}>
-              Default
-            </Button>
-          </div>
-          <div className={classes.btn}>
-            <span>&lsaquo;button size="lg" /&rsaquo;</span>
-            <Button className={`${classes.lg} ${classes.primary}`}>
-              Default
-            </Button>
-          </div>
-        </div>
-        <div className={classes["btn-box"]}>
-          <div className={classes.btn}>
-            <span>&lsaquo;button color="default" /&rsaquo;</span>
-            <Button className={classes.default}>Default</Button>
-          </div>
-          <div className={classes.btn}>
-            <span>&lsaquo;button color="primary" /&rsaquo;</span>
-            <Button className={classes.primary}>Default</Button>
-          </div>
-          <div className={classes.btn}>
-            <span>&lsaquo;button color="secondary" /&rsaquo;</span>
-            <Button className={classes.secondary}>Secondary</Button>
-          </div>
-          <div className={classes.btn}>
-            <span>&lsaquo;button color="danger" /&rsaquo;</span>
-            <Button className={classes.danger}>Danger</Button>
-          </div>
-        </div>
-        <div className={classes["btn-box"]}>
-          <div className={classes.btn}>
-            <span>&lsaquo;button color="default" /&rsaquo;</span>
-            <Button className={classes['default-hover']}>Default</Button>
-          </div>
-          <div className={classes.btn}>
-            <span>&lsaquo;button color="primary" /&rsaquo;</span>
-            <Button className={classes['primary-hover']}>Default</Button>
-          </div>
-          <div className={classes.btn}>
-            <span>&lsaquo;button color="secondary" /&rsaquo;</span>
-            <Button className={classes['secondary-hover']}>Secondary</Button>
-          </div>
-          <div className={classes.btn}>
-            <span>&lsaquo;button color="danger" /&rsaquo;</span>
-            <Button className={classes['danger-hover']}>Danger</Button>
-          </div>
-        </div>
-      </div>
+      <h1>Buttons</h1>
+      {`<button/>`}
+      <Button></Button>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button variant = 'outline'/>`}
+      <Button variant = 'outline'></Button>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button variant = 'text'/>`}
+      <Button variant = 'text'/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button disableShadow />`}
+      <Button disableShadow/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button disabled>`}
+      <Button className= "disable" disabled/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button variant = 'text' disabled/>`}
+      <Button variant = 'text' disabled/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button startIcon = 'local-grocery-store'/>`}
+      <Button startIcon = 'icon'/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button endIcon = 'local-grocery-store'/>`}
+      <Button endIcon = 'icon'/>
+      <br></br>
+      <br></br>
+      <br></br>
+      
+      {`<button size="sm"/>`}
+      <Button  size="sm"/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button size="md"/>`}
+      <Button size="md"/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button size="lg"/>`}
+      <Button size="lg"/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button color="default"/>`}
+      <Button color="default"/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button color="primary"/>`}
+      <Button color="primary"/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button color="secondary"/>`}
+      <Button color="secondary"/>
+      <br></br>
+      <br></br>
+      <br></br>
+      {`<button color="danger"/>`}
+      <Button  color="danger"/>
     </div>
   );
 }
--- src/UI/Button.js
+++ src/UI/Button.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import classes from "./Button.module.css";
+import React from "react";
+import"./Button.css";
+import { BsFillCartPlusFill } from "react-icons/bs";
 
-const Button = (props) =>{
-    return <button className={`${classes.button} ${props.className}`}>
-        {props.children}
+const VARIANT = ["outline", "text"];
+
+const SIZES = ["sm", "md", "lg"];
+
+const STYLES = ["default", "primary", "secondary", "danger"];
+
+
+
+const Button = (props) => {
+  const { variant, startIcon = '', endIcon = '', size, color, disableShadow, disabled } =
+    props;
+  const checkButtonVariant = VARIANT.includes(variant) ? ("btn--" + variant) : "";
+  const checkButtonStartIcon = (startIcon !== "" ? "btn--startIcon" : "");
+  const checkButtonEndIcon = (endIcon !== "" ? "btn--endIcon" : "");
+  const checkButtonSize = SIZES.includes(size) ? ("btn--" + size) : "";
+  const checkButtonColor = STYLES.includes(color) ? "btn--" + color : "btn--default";
+  const checkButtonDisableShadow = disableShadow != null ? "btn--disableShadow" : "";
+  const checkButtonDisabled = disabled != null ? true : false;
+
+  return <button
+      type="button"
+      className={`btn ${checkButtonColor} ${checkButtonSize} ${checkButtonVariant} ${checkButtonDisableShadow} ${checkButtonStartIcon} ${checkButtonEndIcon}`}
+      disabled={checkButtonDisabled}
+    >
+      {checkButtonStartIcon !== "" && (
+        <span>
+          <BsFillCartPlusFill/>
+        </span>
+      )}
+      Default
+      {checkButtonEndIcon !== "" && (
+        <span>
+          <BsFillCartPlusFill/>
+        </span>
+      )}
     </button>
-}
+  ;
+};
 
 export default Button;
